b035cf3869bf515b6a1d4d0f03ca0578d377f63e
Erwin De Jesus <dejesuserwin1@gmail.com>
2026-06-22 20:36:21 +0800

    fix structuresss

--- alpha-frontend/app/customer/layout.tsx
+++ alpha-frontend/app/customer/layout.tsx
@@ -1,9 +1,18 @@
+import "./globals.css";
 import AppShell from "@/components/layout/AppShell";
 
-export default function CustomerLayout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <AppShell>{children}</AppShell>;
+  return (
+    <html>
+      <body>
+        <AppShell>
+          {children}
+        </AppShell>
+      </body>
+    </html>
+  );
 }
