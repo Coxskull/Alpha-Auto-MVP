997f0bd28d5754c5444780379c363420109c8282
Erwin De Jesus <dejesuserwin1@gmail.com>
2026-06-22 20:20:08 +0800

    fix structuress

--- alpha-frontend/app/provider/services/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL:
-    process.env.NEXT_PUBLIC_API_URL ||
-    "https://alpha-backend-production-673a.up.railway.app/api",
-});
-
-export default api;
--- alpha-frontend/services/api.ts
+++ alpha-frontend/services/api.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.NEXT_PUBLIC_API_URL ||
+  "https://alpha-backend-production-673a.up.railway.app";
+
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL,
 });
 
 export default api;
